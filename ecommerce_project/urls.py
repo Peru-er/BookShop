c92d648d51f069e0435b2f14343ef68ac23ba5f9
Peru-er <shynkar.snizhana@gmail.com>
2026-07-11 00:41:31 +0200

    Do not ask

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -28,12 +28,8 @@ urlpatterns = [
     path('', include('shop.urls', namespace='shop')),
 ]
 
-<<<<<<< HEAD
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
-=======
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> production
