a42847b085a7e622e56ef3692a7ab1ac0eae226b
Peru-er <shynkar.snizhana@gmail.com>
2026-07-10 20:09:34 +0200

    Added Book Dimensions

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -157,11 +157,10 @@ STORAGES = {
 CART_SESSION_ID = 'cart'
 AUTH_USER_MODEL = 'users.CustomUser'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = env('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
